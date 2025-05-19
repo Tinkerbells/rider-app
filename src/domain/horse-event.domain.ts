52c17e90b7453afa4d752f28be0a740c744a814b
tinkerbells <daniel.kasatkin.work@gmail.com>
2025-05-19 16:03:03 +0300

    feat: remove task enum for storage version

--- src/domain/horse-event.domain.ts
+++ src/domain/horse-event.domain.ts
@@ -1,16 +1,10 @@
+import type { Task } from './task.domain'
 import type { Horse } from './horse.domain'
-
-export enum HorseEventTasks {
-  COLLECT = 'collect', // собрать
-  DISASSEMBLE = 'disassemble', // разобрать
-  WALK = 'walk', // выгулить
-  CUSTOM = 'custom', // пользовательский тип
-}
 
 export interface HorseEvent {
   id: string | number
   horse: Horse
-  tasks: HorseEventTasks[]
+  tasks: Task[]
   name?: string
   time: string
   date: string
